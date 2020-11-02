e5ee12ae7015cdb0ed18b8ec6d823de11aad6516
Bunnasorn Kaewsiri <bunnasorn.k@ku.th>
2020-11-02 15:06:23 +0700

    Push to github

--- js/main.js
+++ js/main.js
@@ -1,7 +1,75 @@
 const app = new Vue({
     el: '#app',
-    data: {
-        name: "Bunnasorn Kaewsiri",
-        id: "00001"
+    data() {
+        return {
+            students: [],
+            id: '',
+            student: [],
+            works: {
+                "portfolio": "PowerApps: Portfolio",
+                "bmi": "PowerApps: BMI Calculator",
+                "comp_design": "Exercise: Computer Design",
+                "cc_license": "Exercise: CC License",
+                "excel": "Exercise: Excel",
+                "tree": "Exercise: Examinature"
+            }
+        }
+    },
+    computed: {
+        currentStudent() {
+            return this.students.filter(std => std.id.startsWith(this.id)).slice(0, 3);
+        },
+        profileColor() {
+            return this.student.profileImage;
+        },
+        missingWorks() {
+            let missing = [];
+            for (work in this.works) {
+                if (!(work in this.student)) {
+                    missing.push(work);
+                }
+            }
+            return missing;
+        },
+        final_score() {
+            return parseFloat(this.student.final_exam).toFixed(2);
+        },
+        total_score() {
+            let total = 0;
+
+            for (work in this.works) {
+                if (work in this.student) {
+                    total += parseFloat(this.student[work]);
+                }
+            }
+
+            return total + parseFloat(this.final_score);
+        },
+        grading() {
+            const ts = this.total_score
+            if (ts >= 80) {
+                return 4
+            } else if (ts >= 75) {
+                return 3.5
+            } else if (ts >= 70) {
+                return 3
+            } else if (ts >= 65) {
+                return 2.5
+            } else if (ts >= 60) {
+                return 2
+            } else if (ts >= 55) {
+                return 1.5
+            } else if (ts >= 50) {
+                return 1
+            } else {
+                return 0
+            }
+        }
+    },
+    mounted() {
+        axios.get('https://gist.githubusercontent.com/3xbun/91344e52ce242e2a3b9fba02a8e9972f/raw/3669ab54a7110e33438ebacf04c7d3d53b310951/students.json').then(res => (this.students = res.data['m2']))
     }
+    
 })
+
+Vue.config.devtools = true
